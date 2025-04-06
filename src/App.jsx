691ae7561884f3106a03c3baa97a1798541562bb
Anurag Kumar <anurag1712002@gmail.com>
2025-04-07 05:11:25 +0530

    parallax Done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.scss"
 import Navbar from './components/navbar/Navbar'
 import Hero from './components/hero/Hero'
 import Parallax from './components/parallax/Parallax'
+import Experience from './components/experience/Experience'
 const App = () => {
   return (
     <div>
@@ -11,7 +12,7 @@ const App = () => {
         <Hero/>
       </section>
       <section id="Experience"><Parallax type="experience"/></section>
-      <section>Experience</section>
+      <section><Experience/></section>
       <section id="Portfolio"><Parallax type="portfolio"/></section>
       <section>Portfolio1</section>
       <section>Portfolio2</section>
---
+++ src/components/experience/Experience.jsx
@@ -0,0 +1,15 @@
+import "./experience.scss"
+
+const Experience = () => {
+  return (
+    <div className="experience">
+        <div className="textContainer">
+            <p>I focus on building reliable websites <br /> and intuitive interface</p>
+        </div>
+        <div className="tittleContainer"></div>
+        <div className="listContainer"></div>
+    </div>
+  )
+}
+
+export default Experience
